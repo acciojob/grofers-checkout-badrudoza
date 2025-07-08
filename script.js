e81593b6b3222d5bbcbaaf721cc399f3fedd255e
badrudoza <106438583+badrudoza@users.noreply.github.com>
2025-07-09 04:10:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -9,14 +9,22 @@ const getSum = () => {
   for (const price of priceElements) {
     total += parseFloat(price.textContent);
   }
-let newrow=document.createElement("tr");
-let newcell=document.createElement("td");
-	newrow.appendChild(newcell);
-	newcell.colSpan=2;
-	newcell.textContent=`Total Price: ${total}`;
-	newcell.style.fontWeight="bold";
-	newcell.style.textAlign="center";
-	document.querySelector("table").appendChild(newrow);
+// let newrow=document.createElement("tr");
+// let newcell=document.createElement("td");
+// 	newrow.appendChild(newcell);
+// 	newcell.colSpan=2;
+// 	newcell.textContent=`Total Price: ${total}`;
+// 	newcell.style.fontWeight="bold";
+// 	newcell.style.textAlign="center";
+// 	document.querySelector("table").appendChild(newrow);
+	let result = document.getElementById("ans");
+if (!result) {
+  result = document.createElement("p");
+  result.id = "ans"; // Required for Cypress
+  document.body.appendChild(result);
+}
+result.textContent = "Total Price: Rs " + total;
+
 	getSumBtn.disabled=true;
 };
 
